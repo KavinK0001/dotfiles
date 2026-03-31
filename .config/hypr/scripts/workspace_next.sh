d4f1affd6b5812d22cf8b574e4a66a44a72347cc
Kavin Kishore <deadeyegam@gmail.com>
2026-03-31 21:50:10 +0530

    updated hyprland.conf

--- .config/hypr/scripts/workspace_next.sh
+++ .config/hypr/scripts/workspace_next.sh
@@ -3,6 +3,6 @@ current=$(hyprctl activeworkspace -j | jq '.id')
 
 if [ "$current" -ge 10 ]; then
     hyprctl dispatch workspace 1
-else
+elif [ "$current" -lt 9 ]; then
     hyprctl dispatch workspace r+1
 fi
